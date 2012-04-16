07fb311d7ffa71bb12ee5e4eb3ffa17f9547494f
Taku Yamada <t-yamada@groovenauts.jp>
2012-04-16 18:27:57 +0900

    rake db:migrate

--- migration/db/schema.rb
+++ migration/db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120416085950) do
+ActiveRecord::Schema.define(:version => 20120416090130) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "part_number"
+    t.decimal  "price"
   end
 
 end
