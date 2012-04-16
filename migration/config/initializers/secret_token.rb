6e3deaa0c5a3f60dd7c47db720d4e41bc489b8df
Taku Yamada <t-yamada@nautilus-technologies.com>
2012-04-16 17:49:39 +0900

    rails new migration

---
+++ migration/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Migration::Application.config.secret_token = 'b9fb028e94e50327c30e97986dac2319256ced8ea2659155098af87155fba075746efed085e5e05205516c11580016ed7dbac34f50b3af79ce4aa22605a1a6e4'
