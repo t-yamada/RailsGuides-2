2af05bac1cae08bf54b537d5c942b011ec65a41f
Taku Yamada <t-yamada@groovenauts.jp>
2012-04-16 18:27:33 +0900

    rake db:rollback

--- migration/db/schema.rb
+++ migration/db/schema.rb
@@ -11,15 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120416090130) do
+ActiveRecord::Schema.define(:version => 20120416085950) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.string   "part_number"
-    t.decimal  "price"
   end
 
 end
